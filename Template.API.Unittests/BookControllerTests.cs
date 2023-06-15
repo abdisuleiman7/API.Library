173bbcd5bc9510bb3d91e02cea14d8a40cfa2f1a
abdisuleiman7 <abdifatah.suleiman7@gmail.com>
2023-06-15 23:44:23 +0200

    add happy flow tests

--- Template.API.Unittests/BookControllerTests.cs
+++ Template.API.Unittests/BookControllerTests.cs
@@ -1,14 +1,10 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Template.API.Core.Interfaces;
 using Template.API.Core.Models;
-using Template.API.Infrastructure.Data;
-using Template.API.Infrastructure.Providers;
 using Template.API.Rest.Controllers;
-using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Template.API.Unittests
 {
@@ -21,8 +17,8 @@ namespace Template.API.Unittests
         {
             _bookProviderMock = new Mock<IBookProvider>();
             _bookProviderLoggerMock = new Mock<ILogger<BooksController>>();
-            _sut = new BooksController(_bookProviderMock.Object,_bookProviderLoggerMock.Object);
-            
+            _sut = new BooksController(_bookProviderMock.Object, _bookProviderLoggerMock.Object);
+
         }
         [Fact]
         public void GetBooksByTitle_ReturnsOkResult()
@@ -35,21 +31,63 @@ namespace Template.API.Unittests
                 PageAmount= 100,
                 ISBN= 123456789
             } });
-            
+
 
 
             //// Act
             var result = _sut.GetBooksByTitle("Title");
-            
+
+            var resultObj = (result.Result as OkObjectResult).Value as List<Book>;
             //// Assert
-            result.Result.Should().BeOfType<OkObjectResult>();
-            //result.Result.Should().BeOfType<>();
-            //result.Result[0].Title.Should().Be("Test");
-            //result.Result[0].Author.Should().Be("Test");
-            //result.Result[0].Genre.Should().Be(Genre.Fantasy);
-            //result.Result[0].Language.Should().Be("English");
-            //result.Result[0].PageAmount.Should().Be(100);
-            //result.Result[0].ISBN.Should().Be(123456789);
+            resultObj.Should().BeOfType<List<Book>>();
+
+            resultObj[0].Title.Should().Be("Test");
+            resultObj[0].Author.Should().Be("Test");
+            resultObj[0].Genre.Should().Be(Genre.Fantasy);
+            resultObj[0].Language.Should().Be("English");
+            resultObj[0].PageAmount.Should().Be(100);
+            resultObj[0].ISBN.Should().Be(123456789);
+            resultObj.Count.Should().Be(1);
+            
+        }
+
+
+        [Fact]
+        public void GetAllBooks_ReturnsOkResult()
+        {
+            _bookProviderMock.Setup(x => x.GetAllBooks()).ReturnsAsync(new List<Book>() { new Book {
+                Title= "Test",
+                Author= "Test",
+                Genre= Genre.Fantasy,
+                Language= "English",
+                PageAmount= 100,
+                ISBN= 123456789
+            }, new Book{
+                Title= "Test2",
+                Author= "Test2",
+                Genre= Genre.Detective,
+                Language= "English",
+                PageAmount= 1000,
+                ISBN= 1234567890
+            } });
+
+
+            var result = _sut.GetAllBooks();
+            var resultObj = (result.Result as OkObjectResult).Value as List<Book>;
+            resultObj.Count.Should().Be(2);
+            resultObj[0].Title.Should().Be("Test");
+            resultObj[0].Author.Should().Be("Test");
+            resultObj[0].Genre.Should().Be(Genre.Fantasy);
+            resultObj[0].Language.Should().Be("English");
+            resultObj[0].PageAmount.Should().Be(100);
+            resultObj[0].ISBN.Should().Be(123456789);
+            resultObj[1].Title.Should().Be("Test2");
+            resultObj[1].Author.Should().Be("Test2");
+            resultObj[1].Genre.Should().Be(Genre.Detective);
+            resultObj[1].Language.Should().Be("English");
+            resultObj[1].PageAmount.Should().Be(1000);
+            resultObj[1].ISBN.Should().Be(1234567890);
         }
     }
 }
+    
